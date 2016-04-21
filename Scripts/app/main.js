f6c28cabca7a6da9f701de38c4810896c0bb88f3
justincaselow <justincaselow@gmail.com>
2016-04-21 18:06:10 +0100

    Added Angular (core, routing, animate) and several pages, implemented SPA using routing

---
+++ Scripts/app/main.js
@@ -0,0 +1,22 @@
+﻿var taylorHarrowApp = angular.module('taylorHarrowApp', ['ngRoute', 'ngAnimate']);
+
+taylorHarrowApp.controller('mainController', function ($scope) {
+});
+
+taylorHarrowApp.config(function ($routeProvider) {
+    $routeProvider
+
+        .when('/', {
+            templateUrl: 'pages/home.html',
+            controller: 'mainController'
+        })
+        .when('/Services', {
+            templateUrl: 'pages/services.html'
+        })
+        .when('/AboutUs', {
+            templateUrl: 'pages/aboutUs.html'
+        })
+        .when('/Contact', {
+            templateUrl: 'pages/contact.html'
+        });
+});
